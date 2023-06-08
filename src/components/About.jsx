0dccac8afeb19743b5afb443cc972769ca981c35
dakshMukul <105584413+dakshMukul@users.noreply.github.com>
2023-06-08 12:40:25 +0530

    added about componenet

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
@@ -7,29 +7,33 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ServiceCard = ({ index, title, icon }) => {
-  return (
-    // < className="xs:w-[250px] w-full">
-      <motion.div
-        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-        className="xs:w-[250px] w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
-        <div options={{ max: 45, scale: 1, speed: 450,}}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
-          <img
-            src={icon}
-            alt="web-development"
-            className="w-16 h-16 object-contain"
-          />
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
 
-          <h3 className="text-white text-[20px] font-bold text-center">
-            {title}
-          </h3>
-        </div>
-      </motion.div>
-  );
-};
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </Tilt>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
@@ -41,7 +45,7 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[16px] max-w-3xl leading-[30px]"
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
@@ -50,7 +54,7 @@ const About = () => {
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10 justify-evenly">
+      <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
