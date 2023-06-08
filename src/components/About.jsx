58e4342d0814dae3472d6722e7d74556a31e8126
dakshMukul <105584413+dakshMukul@users.noreply.github.com>
2023-06-08 12:21:27 +0530

    titl fixing in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,10 +9,10 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    // < className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        className="xs:w-[250px] w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div options={{ max: 45, scale: 1, speed: 450,}}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
@@ -28,7 +28,6 @@ const ServiceCard = ({ index, title, icon }) => {
           </h3>
         </div>
       </motion.div>
-    </Tilt>
   );
 };
 
