0ce14f034dc2b7b14d6aa4b6420c8eab2b912c45
dakshMukul <105584413+dakshMukul@users.noreply.github.com>
2023-06-08 12:14:01 +0530

    titl fixing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,12 +14,7 @@ const ServiceCard = ({ index, title, icon }) => {
         variants={fadeIn("right", "spring", index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
-        <div
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
+        <div options={{ max: 45, scale: 1, speed: 450,}}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img
@@ -28,12 +23,14 @@ const ServiceCard = ({ index, title, icon }) => {
             className="w-16 h-16 object-contain"
           />
 
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
-  )
-}
+  );
+};
 
 const About = () => {
   return (
@@ -54,7 +51,7 @@ const About = () => {
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10 justify-evenly" >
+      <div className="mt-20 flex flex-wrap gap-10 justify-evenly">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
